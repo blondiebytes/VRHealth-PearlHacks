914b3d94b0115f6628b8dea5893bc0afb662bed8
Kathryn Hodge <kahodge@vassar.edu>
2016-04-03 08:18:24 -0400

    Adding walls and another location

--- HealthVR/Assets/OVR/Scripts/GestureHands.cs
+++ HealthVR/Assets/OVR/Scripts/GestureHands.cs
@@ -9,7 +9,7 @@ public class GestureHands : MonoBehaviour {
 	public GameObject pointTwo;
 	public GameObject cameraVR;
 	public GameObject hands;
-	bool hasTraveled;
+	int whereAt;
 	int isReadyToChange;
 
 	// Use this for initialization
@@ -17,7 +17,7 @@ public class GestureHands : MonoBehaviour {
 		controller = new Controller ();
 		controller.EnableGesture (Gesture.GestureType.TYPE_SWIPE);
 		controller.EnableGesture (Gesture.GestureType.TYPE_KEY_TAP);
-		hasTraveled = false;
+		whereAt = 0;
 		isReadyToChange = 0;
 		// controller.Config.SetFloat ("Gesture.Swipe.MinLength", 200.0f);
 		// controller.Config.SetFloat("Gesture.Swipe.MinVelocity", 750f);
@@ -33,21 +33,21 @@ public class GestureHands : MonoBehaviour {
 				Gesture g = gestures [i];
 				if (g.Type == Gesture.GestureType.TYPE_KEY_TAP) {
 					Debug.Log ("Key Tap");
-					if (hasTraveled) {
+					if (whereAt == 0) {
 						// -0.72, -0.97, 0.81
 						// 0, 0, 0
 						//Quaternion.
 						//hands.gameObject.transform.position = new Vector3(-0.72f, -0.97f, 0.81f);
 						cameraVR.gameObject.transform.position = new Vector3 (pointOne.transform.position.x,
 							pointOne.transform.position.y, pointOne.transform.position.z);
-						hasTraveled = false;
-					} else {
+						whereAt++;
+					} else if (whereAt == 1) {
 						// 0.72, 0.97, -0.81
 						// 0, 90, 0
 						//hands.gameObject.transform.position = new Vector3(0.72f, 0.97f, -0.81f);
 						cameraVR.transform.position = new Vector3 (pointTwo.transform.position.x,
 							pointTwo.transform.position.y, pointTwo.transform.position.z);
-						hasTraveled = true;
+						whereAt++;
 					}
 				} else if (g.Type == SwipeGesture.ClassType ()) {
 					// transition to other scene
